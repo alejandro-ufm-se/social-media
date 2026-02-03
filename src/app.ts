74197d11fff1ac8c9d84cdda674488e2ec6b54bc
JoseGuzmanZamora <joseguzmanzamora@gmail.com>
2026-02-03 08:14:04 -0600

    Clean up app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,58 +10,12 @@ const port: number = config.port;
 app.use(express.json())
 
 app.use((_req, _res, _next) => {
-    console.log(`Time ${Date.now()}!`);
-    console.log('Hello from the app level middleware');
+    console.log(`Time Log ${Date.now()}!`);
     _next();
-}, 
-(_req, _res, _next) => {
-    console.log(`Hola! desde el segundo middleware ${Date.now()}!`);
-    _next();
-}
+    },
 );
 
-
-// app.METHOD(PATH, HANDLER)
-app.get('/', (uno, _res) => {
-    // TODO: code
-    _res.send('Hello World!');
-})
-
-interface LoginModel {
-    email: string;
-    password: string;
-    name: string;
-}
-
-// Query params example
-app.post('/login', (_req, _res) => {
-    let email: any = _req.query.email;
-    let password: any = _req.query.password;
-    console.log(email);
-    console.log(password);
-
-    _res.send(`Received email ${email} and password ${password}`);
-})
-
-// Body example
-app.post('/v2/login', (_req, _res) => {
-    let body: LoginModel = _req.body;
-
-    let email: string = body['email'];
-    let password: string = body['password'];
-    let name: string = body['name'];
-
-    _res.send(`Received email ${email} and password ${password}, my name is ${name}`);
-})
-
-// Path params example
-app.get('/name/:example', (_req, _res) => {
-    console.log(_req.params);
-    _res.send(`Hi my name is ${_req.params.example}`);
-})
-
 app.use('/user', userRoutes);
-
 
 // Error Middleware
 app.use((_err: Error, _req: Request, _res: Response, _next: NextFunction) => {
@@ -69,9 +23,6 @@ app.use((_err: Error, _req: Request, _res: Response, _next: NextFunction) => {
     _res.status(500).send(_err.message);
 });
 
-
 app.listen(port, () => {
-    console.log(config.dbUserName);
-    console.log(config.dbPassword);
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Social media app listening on port ${port}`);
 })
