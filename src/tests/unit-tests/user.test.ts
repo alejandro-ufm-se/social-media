98ca496da2685c5afd1053f66ced949631c21600
JoseGuzmanZamora <joseguzmanzamora@gmail.com>
2026-03-25 18:06:16 -0600

    Fix base user tests

--- src/tests/unit-tests/user.test.ts
+++ src/tests/unit-tests/user.test.ts
@@ -13,12 +13,12 @@ describe('UserService', () => {
     });
 
     // el password debe de estar encriptado
-    expect(result.password).toEqual("encrypted1234");
+    expect(result.email).toEqual("encrypted1234");
 
     // el id tiene que ser mayor a 0
     expect(result.id).toBeGreaterThan(0);
 
     // el array de amigos tiene que estar vacio
-    expect(result.friends.length).toBe(0);
+    expect(result.email.length).toBe(0);
   });
 });
